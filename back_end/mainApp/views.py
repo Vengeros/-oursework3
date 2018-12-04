55445ef5647a333c269efc6f9afaa23a1ed55a8e
vengeros <vengerhrum@gmail.com>
2018-12-04 22:24:02 +0200

    rm evn

--- back_end/mainApp/views.py
+++ back_end/mainApp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'front_end/index.html')
+
+def help_button(request):
+    return render(request,'front_end/index.html',{'values':['Questions by tv-phone','(666) 666-66-66']})
--- front_end/js/login.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#log-in").click(function() {
-    $(".transparent-background").show();
-  });
-  $(".transparent-background").click(function() {
-    $(".transparent-background").hide();
-  });
-});
