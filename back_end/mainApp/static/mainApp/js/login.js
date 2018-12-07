91893f8721fc0148912f3ea7583982d877b1abd2
vengeros <vengerhrum@gmail.com>
2018-12-07 14:26:08 +0200

    added venv and it probably works

--- back_end/mainApp/static/mainApp/js/login.js
+++ back_end/mainApp/static/mainApp/js/login.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $("#log-in").click(function() {
-    $(".transparent-background").show();
-    $(".transparent-background").css("display", "grid")
+    // $(".transparent-background").show();
+    $(".transparent-background").css("display", "grid");
     // $(".registration").css("width","700px")
   });
   $(".transparent-area").click(function() {
-    $(".transparent-background").hide();
+    // $(".transparent-background").hide();
+    $(".transparent-background").css("display", "none")
   });
 });
--- back_end/mainApp/views.py
+++ back_end/mainApp/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request,'front_end/index.html',{'help':'help/'})
 
 def help_button(request):
-    return render(request,'front_end/index.html',{'values':['Questions by tv-phone','(666) 666-66-66'],'help':''})
+    return render(request,'front_end/index.html',{'values':['Questions by tv-phone','(666) 666-66-66'],'help':'../'})
