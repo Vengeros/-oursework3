6034fb9c25e8437f67bef2e6d894a9374d2ea433
Kamikadze4GAME <osskrisha@gmail.com>
2018-12-14 23:50:32 +0200

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+{
+  "apps": [
+    {
+      "name": "Coursework3",
+      "cwd": "/home/vova/Coursework3/back_end",
+      "args": "runserver 0.0.0.0:8000",
+      "script": "manage.py",
+      "exec_mode": "fork",
+      "exec_interpreter": "python3"
+    }
+  ]
+}
